8ee59883f0145c8b1da5f823374a6789f4c3bc80
atidele <alejandro.tidele@mercadolibre.com>
2020-10-11 21:09:03 -0300

    Updates

---
+++ precache-manifest.9a22b8ea6baea5169af3203c66e9ecf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daebe7d6a2e99f8f48242d1ab4c10006",
+    "url": "/van-hack/index.html"
+  },
+  {
+    "revision": "0a0bc2b4d3cab14a2d30",
+    "url": "/van-hack/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "be2cdd250ee52383f0c1",
+    "url": "/van-hack/static/js/2.e6d03bdf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/van-hack/static/js/2.e6d03bdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a0bc2b4d3cab14a2d30",
+    "url": "/van-hack/static/js/main.43df673d.chunk.js"
+  },
+  {
+    "revision": "3a265dcf0b07fab49511",
+    "url": "/van-hack/static/js/runtime-main.6ea472e2.js"
+  }
+]);
